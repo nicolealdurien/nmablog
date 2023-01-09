fb8a5b30516ab50bd8f4964403488872ff76de9c
nicolealdurien <nicole@lumenlearning.com>
2023-01-09 10:42:39 -0500

    adds blog entry cards

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,3 @@
-// import type { AppProps } from 'next/app';
-// import React from 'react';
-// import { Layout } from '../components';
-// import '../styles/globals.scss';
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <Layout>
-//       <Component {...pageProps} />
-//     </Layout>
-//   );
-// }
-
-// export default MyApp;
-
 import React from 'react';
 
 import { Layout } from '../components';
